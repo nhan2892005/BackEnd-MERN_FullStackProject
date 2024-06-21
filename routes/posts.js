4493bb8e21fa3e99b19a684a614c024ddb92e228
Phúc <nhan.nguyen2005phuyen@hcmut.edu.vn>
2024-06-22 00:50:08 +0700

    update actions for google users

--- routes/posts.js
+++ routes/posts.js
@@ -5,9 +5,9 @@ const router = express.Router();
 import auth from '../middleware/auth.js';
 
 router.get('/', getPosts);
-router.post('/', auth,  createPost);
-router.patch('/:id', auth, updatePost);
-router.delete('/:id', auth, deletePost);
-router.patch('/:id/likePost', auth, likePost);
+router.post('/', createPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
